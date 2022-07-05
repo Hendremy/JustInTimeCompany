ffb8e2003096d267d4d262536004a898bb221245
HENDRICE_Rémy <r.hendrice@student.helmo.be>
2022-07-05 16:10:08 +0200

    Début DbContext

---
+++ JustInTimeCompany/Models/JITCDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JustInTimeCompany.Models
+{
+    public class JITCDbContext : DbContext
+    {
+        public JITCDbContext(DbContextOptions<JITCDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Booking>().HasKey(b => new { b.FlightId, b.CustomerId });
+
+            Seed(modelBuilder);
+        }
+
+        private void Seed(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public DbSet<Booking> Bookings { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Aircraft> Aircrafts { get; set; }
+
+        public DbSet<AircraftModel> AircraftModels { get; set; }
+
+        public DbSet<Airport> Airports { get; set; }
+
+        public DbSet<Pilot> Pilots { get; set; }
+
+        public DbSet<Flight> Flights { get; set; }
+    }
+}
--- JustInTimeCompany/Program.cs
+++ JustInTimeCompany/Program.cs
@@ -1,7 +1,16 @@
+using JustInTimeCompany.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddMvc(options => options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute()));
+
+builder.Services.AddDbContext<JITCDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("default")));
 
 var app = builder.Build();
 
