9215bd7d105f5e0ed5ec5bc57e0b0fc0bb5268aa
HENDRICE_Rémy <r.hendrice@student.helmo.be>
2022-07-09 12:29:54 +0200

    Début vue des Vols & Appareils côté manager

--- JustInTimeCompany/Controllers/FlightController.cs
+++ JustInTimeCompany/Controllers/FlightController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JustInTimeCompany.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JustInTimeCompany.Controllers
 {
@@ -7,6 +8,19 @@ namespace JustInTimeCompany.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Flight flight)
+        {
+
+
+            return View(flight); 
         }
     }
 }
