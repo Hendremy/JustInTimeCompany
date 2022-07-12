63b48930730cf5c54b26a9807e949e17a5a5c461
HENDRICE_Rémy <r.hendrice@student.helmo.be>
2022-07-12 17:01:25 +0200

    Avancement form flightreport

--- JustInTimeCompany/Models/Schedule.cs
+++ JustInTimeCompany/Models/Schedule.cs
@@ -1,9 +1,13 @@
-﻿namespace JustInTimeCompany.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JustInTimeCompany.Models
 {
     public class Schedule
     {
         public int Id { get; set; }
+        [Display(Name ="Heure de décollage")]
         public DateTime TakeOff { get; set; }
+        [Display(Name = "Heure d'atterrissage")]
         public DateTime Landing { get; set; }
 
         public Schedule (DateTime takeOff, DateTime landing)
