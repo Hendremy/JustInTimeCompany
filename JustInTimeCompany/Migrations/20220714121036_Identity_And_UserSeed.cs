b97d9040844cfe06f0823c74a67ac43f123e6a58
HENDRICE_Rémy <r.hendrice@student.helmo.be>
2022-07-16 13:08:36 +0200

    Fix Id de booking

--- JustInTimeCompany/Migrations/20220714121036_Identity_And_UserSeed.cs
+++ JustInTimeCompany/Migrations/20220714121036_Identity_And_UserSeed.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace JustInTimeCompany.Migrations
 {
-    public partial class IdentityUser_Et_Seed : Migration
+    public partial class Identity_And_UserSeed : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
