155bf84bb5bfccb31606aa20a8a313ee189f4726
HENDRICE_Rémy <r.hendrice@student.helmo.be>
2022-07-28 19:52:11 +0200

    Début stats

--- JustInTimeCompany/Controllers/StatsController.cs
+++ JustInTimeCompany/Controllers/StatsController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JustInTimeCompany.Models;
+using JustInTimeCompany.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace JustInTimeCompany.Controllers
 {
     public class StatsController : Controller
     {
+        private readonly JITCDbContext _dbContext;
+
+        public StatsController([FromServices] JITCDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var delayedFlights = _dbContext.Flights
+                .Include(fl => fl.Schedule)
+                .Include(fl => fl.FlightReport)
+                .ThenInclude(fr => fr.ActualSchedule)
+                .ToList()
+                .Where(fl => fl.FlightReport.HasDelay);
+
+            var aircrafts = _dbContext.Aircrafts
+                .Include(ac => ac.Model)
+                .Include(ac => ac.FlightInstances)
+                .ThenInclude(fl => fl.Bookings)
+                .ToList();
+
+            return View(new StatsViewModel(delayedFlights, aircrafts));
         }
     }
 }
---
+++ JustInTimeCompany/Models/ViewModels/StatsViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace JustInTimeCompany.Models.ViewModels
+{
+    public class StatsViewModel
+    {
+        public IEnumerable<Flight> DelayedFlights { get; set; }
+        public IEnumerable<Aircraft> Aircrafts { get; set; }
+
+        public StatsViewModel(IEnumerable<Flight> delflight, IEnumerable<Aircraft> aircrafts)
+        {
+            DelayedFlights = delflight;
+            Aircrafts = aircrafts;
+        }
+    }
+}
